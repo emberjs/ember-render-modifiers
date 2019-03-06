17aa8a15e2fd6cbda64ade246f5dc9d23a4b5aab
Gavin Joyce <gavinjoyce@gmail.com>
2019-03-06 11:57:56 +0000

    Added `{{did-insert}}`, `{{did-update}}` and `{{will-destroy}}` API docs

--- addon/modifiers/did-insert.js
+++ addon/modifiers/did-insert.js
@@ -1,5 +1,49 @@
 import Ember from 'ember';
 
+/**
+  The `{{did-insert}}` element modifier is activated when an element is
+  inserted into the DOM.
+
+  In this example, the `fadeIn` function receives the `div` DOM element as its
+  first argument and is executed after the element is inserted into the DOM.
+
+  ```handlebars
+  <div {{did-insert this.fadeIn}} class="alert">
+    {{yield}}
+  </div>
+  ```
+
+  ```js
+  export default Component.extend({
+    fadeIn(element) {
+      element.classList.add('fade-in');
+    }
+  });
+  ```
+
+  By default, the executed function will be unbound. If you would like to access
+  the component context in your function, use the `action` helper as follows:
+
+  ```handlebars
+  <div {{did-insert (action this.incrementCount)}}>first</div>
+  <div {{did-insert (action this.incrementCount)}}>second</div>
+
+  <p>{{this.count}} elements were rendered</p>
+  ```
+
+    ```js
+  export default Component.extend({
+    @tracked count = 0;
+
+    incrementCount() {
+      this.count++;
+    }
+  });
+  ```
+
+  @method did-insert
+  @public
+*/
 export default Ember._setModifierManager(
   () => ({
     createModifier() {},
--- addon/modifiers/will-destroy.js
+++ addon/modifiers/will-destroy.js
@@ -1,5 +1,35 @@
 import Ember from 'ember';
 
+/**
+  The `{{will-destroy}}` element modifier is activated immediately before the element
+  is removed from the DOM.
+
+  ```handlebars
+  <div {{will-destroy this.teardownPlugin}}>
+    {{yield}}
+  </div>
+  ```
+
+  ```js
+  export default Component.extend({
+    teardownPlugin(element) {
+      // teardown logic here
+    }
+  });
+  ```
+
+  By default, the executed function will be unbound. If you would like to access
+  the component context in your function, use the `action` helper as follows:
+
+    ```handlebars
+  <div {{will-destroy (action this.teardownPlugin)}}>
+    {{yield}}
+  </div>
+  ```
+
+  @method will-destroy
+  @public
+*/
 export default Ember._setModifierManager(
   () => ({
     createModifier() {
