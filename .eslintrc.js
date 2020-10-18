660559d635ea8938519564c92244c52cc801b034
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-10-18 10:59:10 -0400

    run ember-cli-update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
+'use strict';
+
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 2018,
     sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   plugins: ['ember', 'prettier'],
   extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
@@ -28,17 +34,13 @@ module.exports = {
       ],
       excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015,
+        sourceType: 'script'
       },
       env: {
         browser: false,
         node: true,
       },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      }),
-    },
-  ],
+      extends: ['plugin:node/recommended']
+    }
+  ]
 };
