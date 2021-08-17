5f50350774312645a605c1f9960d47a910eb2ab8
Dave Laird <kiwiupover@gmail.com>
2021-08-17 12:22:16 -0600

    Adding embroider-safe to the test matrix

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
+const { embroiderSafe } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
@@ -100,7 +100,6 @@ module.exports = async function () {
         },
       },
       embroiderSafe(),
-      embroiderOptimized(),
     ],
   };
 };
