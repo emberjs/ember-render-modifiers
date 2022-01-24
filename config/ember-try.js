3738a70cb2b01ed1a5dd8258a9609b02bea94340
Robert Wagner <rwwagner90@gmail.com>
2022-01-24 10:32:16 -0500

    Add back earlier Ember tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -89,6 +89,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             '@ember/jquery': '^1.1.0',
+            'ember-source': '~3.28.0',
           },
         },
       },
