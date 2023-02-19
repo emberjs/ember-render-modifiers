0028eadeb29d81a2d0b7a633900a9c8ab800e9ba
V. Lascik <vlascik@users.noreply.github.com>
2023-02-19 11:33:39 +0100

    Add modifier types and template-registry

---
+++ addon/modifiers/-private/shared.d.ts
@@ -0,0 +1,10 @@
+import Modifier from '@ember/modifier';
+
+interface RenderModifierSignature<Element extends HTMLElement | SVGElement, Args extends any[]> {
+    Args: { Positional: [(element: Element, args: Args) => any, ...Args] };
+    Element: Element;
+}
+
+export declare class RenderModifier<Element extends HTMLElement | SVGElement, Args extends any[]> extends Modifier<
+    RenderModifierSignature<Element, Args>
+> {}
